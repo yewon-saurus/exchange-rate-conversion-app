bdb9a69b9f3c3032bc40a5dbaace51273a454e2f
yewon-saurus <ywwwon01@gmail.com>
2024-04-16 23:14:36 +0900

    [feat] AmountForm amount 입력란 조건 처리 로직 작성

--- src/components/AmountForm.jsx
+++ src/components/AmountForm.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import styled from "styled-components";
 import { useSelector, useDispatch } from "react-redux";
 
@@ -21,9 +22,17 @@ select {
 const AmountForm = () => {
     const currencyArr = useSelector((state) => state.currency.currency_arr);
 
+    const [amount, setAmount] = React.useState(0);
+
+    const handleInputChange = (e) => {
+        const value = e.target.value;
+        if (value <= 1000) setAmount(Number(value));
+        else setAmount(1000);
+    }
+
     return (
         <AmountFormDiv>
-            <input />
+            <input value={Number(amount).toLocaleString('ko-KR')} onChange={handleInputChange} />
             <select onChange={(e) => console.log(e.target.value)}>
                 {
                     currencyArr.map((ele, idx) => <option key={idx} value={ele}>{ele}</option>)
